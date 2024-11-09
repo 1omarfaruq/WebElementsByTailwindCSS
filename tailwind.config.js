2bc75cfcca0521b8a1ee7b48d6cc0701c4369f85
1omarfaruq <1omarfaruq@gmail.com>
2024-11-09 23:16:25 +0600

    Complete Navbar design 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./navbar/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
